e0a74d8e2715a06aa7f840de97f8df39f3c5769b
tkrsivasakthi <tkrsivasakthi@gmail.com>
2023-04-12 20:12:20 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,40 @@
-import { Component ,OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {User} from './user';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   name = 'Angular';
-response:any;
-user_response:any={};
-login:string;
-id:any;
-avatar_url:any;
-  constructor(private http: HttpClient){
-
-  }
-  ngOnInit(){
-    this.http.get('https://api.github.com/users').subscribe(data=>{
-this.response=data;
-console.log(this.response);
+  response: any;
+  user_response: any = {};
+  login: string;
+  id: any;
+  avatar_url: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http.get('https://api.github.com/users').subscribe((data) => {
+      console.log('sakthi-:', data);
+      this.response = data;
+      console.log(this.response);
     });
   }
-  getUser(user){
+  getUser(user) {
     console.log(user);
-   this.login=user.login;
-    this.id =user.id;
-  this.avatar_url=user.avatar_url;
+    this.login = user.login;
+    this.id = user.id;
+    this.avatar_url = user.avatar_url;
   }
-getName(){
-  if(this.login !=""){
-  this.response=this.response.filter(res=>{
-    console.log(res);
-    return res.login.toLowerCase().match(this.login.toLowerCase());
-  })
-}
-else if( this.login ==""){
-this.ngOnInit();
-}
-
-}
+  getName() {
+    if (this.login != '') {
+      this.response = this.response.filter((res) => {
+        console.log(res);
+        return res.login.toLowerCase().match(this.login.toLowerCase());
+      });
+    } else if (this.login == '') {
+      this.ngOnInit();
+    }
+  }
 }
